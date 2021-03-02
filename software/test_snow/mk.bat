cc87f6772e983a6f9d6992a0339b157ec069bca9
nino-porcino <nino.porcino@gmail.com>
2021-03-02 22:26:23 +0100

    test program for testing snow effect

---
+++ software/test_snow/mk.bat
@@ -0,0 +1,1 @@
+zcc +vz test_snow.c -o test_snow.vz -create-app -Cz--audio
---
+++ software/test_snow/test_snow.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int test_snow() {
+   #asm
+
+   di
+   ld hl, $7000
+   xor a
+
+loop:
+   ld (hl),a
+   jr loop
+
+   #endasm 
+}
+
+
+int main() {   
+   vz_clrscr();
+   printf("\n\nTEST SNOW\n\n");
+   printf("WRITTEN BY NINO PORCINO\n\n");
+   test_snow();
+}
