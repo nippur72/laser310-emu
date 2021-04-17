c890fe43bb43e435dd1963921ea91ef44da2fa1b
nino-porcino <nino.porcino@gmail.com>
2021-04-17 11:45:46 +0200

    improve grbuffer

---
+++ software/test_grbuffer/mk.bat
@@ -0,0 +1,2 @@
+zcc +vz test_grbuffer.c -o test_grbuffer.vz -create-app -Cz--audio -compiler sdcc -SO3 --list
+@del *.cas
--- software/test_grbuffer/test_grbuffer.c
+++ software/test_grbuffer/test_grbuffer.c
@@ -8,8 +8,8 @@ typedef unsigned int word;
 
 byte screen_buffer[SCREEN_SIZE];
 
-#define FIRST_SLICE  SCREEN_SIZE-784
-#define SECOND_SLICE 784
+#define FIRST_SLICE  SCREEN_SIZE-448
+#define SECOND_SLICE 448
 
 void screen_update() __naked {
    __asm
@@ -27,7 +27,25 @@ screen_update_loop1:
    jr c,screen_update_loop1
 
    ; copy first slice during the retrace
-   ldir
+   //ldir
+   fastloop1:
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   jp pe,fastloop1  ; Loop until bc = zero
 
    ; prepare for second slice 
    ld bc, SECOND_SLICE
@@ -39,7 +57,25 @@ screen_update_loop2:
    jr c,screen_update_loop2
 
    ; copy the second slice during the retrace
-   ldir
+   ;ldir
+   fastloop2:
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   ldi
+   jp pe,fastloop2  ; Loop until bc = zero
 
    ei
    ret
@@ -49,28 +85,35 @@ screen_update_loop2:
 void main() {
    vz_mode(1);
 
+   vz_setbase(screen_buffer);
+
    // clears the buffer 
    for(word t=0; t<4096; t++) {
       screen_buffer[t] = (t<<6);
    }
 
-   word pos1=0;  // some lazy 4-pixels sprites 
-   word pos2=0;  //  
+   // draw borders
+   vz_line(  0,  0,127,  0, 3);
+   vz_line(127,  0,127, 63, 3);
+   vz_line(127, 63,  0, 63, 3);
+   vz_line(  0,  63, 0,  0, 3);
+
+   word pos1=16;   // some lazy 4-pixels sprites
+   word pos2=0;    //
 
    // game loop
    while(1) {
       // do some animation on the screen buffer
       screen_buffer[pos1] = 0;
-      screen_buffer[pos2] = 0;
+      screen_buffer[1024+pos2] = 0;
       pos1 = pos1 + 32;
       pos1 = pos1 & 2047; 
       pos2 = pos2 + 1;
-      pos2 = pos2 & 2047; 
+      pos2 = pos2 & 31;
       screen_buffer[pos1] = 255;
-      screen_buffer[pos2] = 0xaa;
+      screen_buffer[1024+pos2] = 0xaa;
 
       // updates the screen 
       screen_update();
    }
 }
-
