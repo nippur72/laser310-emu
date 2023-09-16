1f6a4c3a7d9607f613ad87ab555b98349bcdd52d
nino-porcino <nino.porcino@gmail.com>
2023-09-16 14:48:11 +0200

    fix aspect ratio

--- src/browser.ts
+++ src/browser.ts
@@ -4,7 +4,7 @@ import { getLaser310 } from "./index";
 import { getFileExtension } from "./bytes";
 import { externalLoad } from "./mdawson";
 
-let aspect = 1.4;
+let aspect = 1.3;
 
 function onResize() {
    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
